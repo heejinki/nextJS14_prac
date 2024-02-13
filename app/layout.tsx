595632d0bfcd155e46451cb0caca3e29bb271a9e
orihiro <kimhijin1@naver.com>
2024-02-14 03:56:56 +0900

    Suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Navigation from "../components/navigation";
 import { Metadata } from "next";
+import Navigation from "../components/navigation";
 
 export const metadata: Metadata = {
   title: {
